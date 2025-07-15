99b82d0b90a634ac3c87bc04c370dc1fb80d2a20
Carlson, Joshua <joshua.carlson@intel.com>
2025-07-15 16:56:11 -0700

    Reorganize main

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
 local lowres = require("lowres")
-local scene = require("scenes.scene")
-local gameScene = require("scenes.gameScene")
 local assets = require("assets")
 local assetData = require("assets.manifest")
 local spriteSheet = require("spriteSheet")
+local scene = require("scenes.scene")
+local gameScene = require("scenes.gameScene")
 
 function love.load()
-    lowres.init(128, 128, 5)
+    sprId = 1
 
     assets.load(assetData)
+    lowres.init(128, 128, 5)
+
     love.graphics.setFont(assets["lowresFont"])
-    sprites = spriteSheet.new(assets["mySprites"], 16)
+    mySprites = spriteSheet.new(assets["mySprites"], 16)
 
     scene.switch(gameScene)
-
-    sprId = 1
 end
 
 function love.update(dt)
@@ -23,8 +23,10 @@ end
 
 function love.draw()
     lowres.beginDraw()
+
     scene.draw()
-    sprites:draw(sprId, 64, 64)
+    mySprites:draw(sprId, 64, 64)
+
     lowres.endDraw()
 end
 
